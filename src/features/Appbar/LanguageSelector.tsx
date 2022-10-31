8f9646d1ee4fa40788d26b0730c9ee049e0d753e
Anis <anis.rahman@nfq.asia>
2022-10-31 11:00:26 +0700

    added langugae switch

---
+++ src/features/Appbar/LanguageSelector.tsx
@@ -0,0 +1,85 @@
+import * as React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Dialog from '@mui/material/Dialog';
+import TranslateIcon from '@mui/icons-material/Translate';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { AppbarActions } from './AppbarSlice';
+const Languages = [{
+    name: 'English',
+    code: 'en'
+}, {
+    name: 'ภาษาไทย',
+    code: 'tha'
+}];
+
+interface Language {
+    name: string
+    code: string
+}
+
+export interface DialogProps {
+    open: boolean;
+    selectedValue: Language;
+    onClose: (value: Language) => void;
+}
+
+function DialogBox(props: DialogProps) {
+    const { onClose, selectedValue, open } = props;
+    const appLanguage = useAppSelector(state => {
+       return state.app.language
+    })
+    console.log(appLanguage)
+    const dispatch = useAppDispatch()
+
+    const handleClose = () => {
+        onClose(selectedValue);
+    };
+
+    const handleListItemClick = (value: Language) => {
+        console.log({value})
+        dispatch(AppbarActions.setLanguage(value))
+        onClose(value);
+    };
+
+    return (
+        <Dialog onClose={handleClose} open={open}>
+            <DialogTitle>Change Language</DialogTitle>
+            <List sx={{ pt: 0 }}>
+                {Languages.map((Language) => (
+                    <ListItem selected={Language.code === appLanguage?.code} onClick={() => handleListItemClick(Language)} key={Language.code}>
+                        <ListItemText primary={Language.name} />
+                    </ListItem>
+                ))}
+            </List>
+        </Dialog>
+    );
+}
+
+export default function LanguageSelector() {
+    const [open, setOpen] = React.useState(false);
+    const [selectedValue, setSelectedValue] = React.useState(Languages[1]);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = (value: Language) => {
+        setOpen(false);
+        setSelectedValue(value);
+    };
+
+    return (
+        <div>
+            <TranslateIcon onClick={handleClickOpen} />
+
+            <DialogBox
+                selectedValue={selectedValue}
+                open={open}
+                onClose={handleClose}
+            />
+        </div>
+    );
+}
